9afa439ee712dc5ae9fbec16635dbfe39b99abf5
keenanti <keenanti@keenanti-Wild-Dog-Performance>
2015-11-14 14:28:09 -0600

    Default Current Player test.

--- src/main/java/com/keenanbros/domain/Players.java
+++ src/main/java/com/keenanbros/domain/Players.java
@@ -3,6 +3,16 @@ package com.keenanbros.domain;
 public class Players {
 	int numberOfPlayers = 2;
 
+	int currentPlayer;
+
+	public int getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public void setCurrentPlayer(int currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+
 	public int getNumberOfPlayers() {
 		return numberOfPlayers;
 	}
--- src/test/java/com/keenanbros/domain/PlayersTest.java
+++ src/test/java/com/keenanbros/domain/PlayersTest.java
@@ -8,9 +8,16 @@ import org.junit.Test;
 public class PlayersTest {
 
 	@Test
-	public void test() {
+	public void defaultNumberOfPlayersTest() {
 		Players players = new Players();
 		
 		assertThat(players.getNumberOfPlayers(), is(2));
+	}
+
+	@Test
+	public void defaultCurrentPlayerTest() {
+		Players players = new Players();
+		
+		assertThat(players.getCurrentPlayer(), is(1));
 	}
 }
