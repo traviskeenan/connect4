e880af0666fe5750993aa71ad193187ecfb29397
keenanti <keenanti@keenanti-Wild-Dog-Performance>
2015-11-14 15:45:44 -0600

    Second Drop test.

--- src/main/java/com/keenanbros/domain/Board.java
+++ src/main/java/com/keenanbros/domain/Board.java
@@ -1,6 +1,8 @@
 package com.keenanbros.domain;
 
 public class Board {
+	
+	private int[][] boradArray = new int[6][7];
 
 	public int drop(int columnNumber, int playerNumber) {
 		return 1;
--- src/test/java/com/keenanbros/domain/BoardTest.java
+++ src/test/java/com/keenanbros/domain/BoardTest.java
@@ -7,11 +7,21 @@ import org.junit.Test;
 public class BoardTest {
 
 	@Test
-	public void boardTest() {
+	public void boardFirstDropTest() {
 		Board board = new Board();
 		int positionOfDropped = board.drop(4, 1);
 		
 		assertSame(1, positionOfDropped);
 	}
 
+	@Test
+	public void boardSecondDropTest() {
+		Board board = new Board();
+
+		int positionOfDropped = board.drop(4, 1);
+		assertSame(1, positionOfDropped);
+
+		int positionOfDropped2 = board.drop(4, 2);
+		assertSame(2, positionOfDropped2);
+	}
 }
