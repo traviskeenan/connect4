39fbe7456308ad7940db96d274c61dcd9e464425
baditch73 <mekeenan73@gmail.com>
2015-11-14 15:01:33 -0600

    Taking turns

--- src/main/java/com/keenanbros/domain/Players.java
+++ src/main/java/com/keenanbros/domain/Players.java
@@ -1,30 +1,34 @@
 package com.keenanbros.domain;
 
 public class Players {
-	int numberOfPlayers = 2;
+    int numberOfPlayers = 2;
 
-	int currentPlayer = 1;
+    int currentPlayer = 1;
 
-	public int getCurrentPlayer() {
+    public int getCurrentPlayer() {
 
-		return currentPlayer;
-	}
+        return currentPlayer;
+    }
 
-	public void setCurrentPlayer(int currentPlayer) {
-		this.currentPlayer = currentPlayer;
-	}
+    public void setCurrentPlayer(int currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
 
-	public int getNumberOfPlayers() {
-		return numberOfPlayers;
-	}
+    public int getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
 
-	public void setNumberOfPlayers(int numberOfPlayers) {
-		this.numberOfPlayers = numberOfPlayers;
-	}
+    public void setNumberOfPlayers(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+    }
 
-	public void takeTurn() {
-		currentPlayer = 2;
+    public void takeTurn() {
+        if (currentPlayer == 1) {
+            currentPlayer = 2;
+        } else {
+            currentPlayer = 1;
+        }
 
-	}
+    }
 
 }
